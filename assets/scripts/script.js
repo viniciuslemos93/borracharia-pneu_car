0dfbd798e98914b46c4bc26d484981d9adfcc581
Vinicius <vinycius1993@gmail.com>
2022-12-13 16:59:51 -0300

    Alterado as imagens do site por ícones, melhorando a experiência do usuário

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,15 @@
 function clickMenu() {
     if (itens_menu.style.display == 'block') {
-        itens_menu.style.display = 'none'
+        itens_menu.style.display = 'none'        
     } else {
         itens_menu.style.display = 'block'
-      }
+        itens_menu.style.transition = '2'
+      }      
     }
 function mudouTamanho() {
     if (window.innerWidth >= 576) {
-        itens_menu.style.display = 'block'
+        itens_menu.style.display = 'block'        
     } else {
-        itens_menu.style.display = 'none'
+        itens_menu.style.display = 'none'        
     }
 }
