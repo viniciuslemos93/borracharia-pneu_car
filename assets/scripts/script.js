a7ba22f862915aba0eb552458699eb0382ada1aa
Vinicius <vinycius1993@gmail.com>
2022-12-13 09:20:12 -0300

    separação do script JS do arquivo Index

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+function clickMenu() {
+    if (itens_menu.style.display == 'block') {
+        itens_menu.style.display = 'none'
+    } else {
+        itens_menu.style.display = 'block'
+      }
+    }
